696302267ccc655c5f93df90a0d238d1584f6767
Santhana Pandi <santhanamkavya9@gmail.com>
2026-04-15 22:45:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def exercise(ex_id):
     return render_template('index.html', active_ex=ex_id)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
